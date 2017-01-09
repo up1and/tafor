780d45cc67899d1422ad5d6e8a9ae33bbd972046
piratecb <piratecb@gmail.com>
2017-01-09 23:40:20 +0800

    新增设置窗口天气现象的添加

--- tafor/setting.py
+++ tafor/setting.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from ui import Ui_setting
+from ui import Ui_setting, main_rc
 
 
 class SettingDialog(QtWidgets.QDialog, Ui_setting.Ui_Dialog):
@@ -8,6 +8,34 @@ class SettingDialog(QtWidgets.QDialog, Ui_setting.Ui_Dialog):
     def __init__(self, parent=None):
         super(SettingDialog, self).__init__(parent)
         self.setupUi(self)
+
+        self.setWindowIcon(QtGui.QIcon(':/setting.png'))
+
+        self.bind_signal()
+
+    def bind_signal(self):
+        self.weather1_add_button.clicked.connect(lambda: self.add_weather(1))
+        self.weather2_add_button.clicked.connect(lambda: self.add_weather(2))
+
+        self.weather1_del_button.clicked.connect(lambda: self.del_weather(1))
+        self.weather2_del_button.clicked.connect(lambda: self.del_weather(2))
+
+    def add_weather(self, which):
+        if which == 1:
+            weather = self.weather1.text()
+            if weather:
+                self.weather1_list.addItem(weather)
+        else:
+            weather = self.weather2.text()
+            if weather:
+                self.weather2_list.addItem(weather)
+
+    def del_weather(self, which):
+        if which == 1:
+            item = self.weather1_list.takeItem(self.weather1_list.currentRow())
+        else:
+            item = self.weather2_list.takeItem(self.weather2_list.currentRow())
+        item = None
 
 
 
