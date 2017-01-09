21f4b555af0018d63d8486b6fc584938f7e3d36f
piratecb <piratecb@gmail.com>
2017-01-09 23:57:34 +0800

    设置窗口可以添加或删除天气现象

--- tafor/setting.py
+++ tafor/setting.py
@@ -14,27 +14,20 @@ class SettingDialog(QtWidgets.QDialog, Ui_setting.Ui_Dialog):
         self.bind_signal()
 
     def bind_signal(self):
-        self.weather1_add_button.clicked.connect(lambda: self.add_weather(1))
-        self.weather2_add_button.clicked.connect(lambda: self.add_weather(2))
+        self.weather1_add_button.clicked.connect(lambda: self.add_weather('weather1'))
+        self.weather2_add_button.clicked.connect(lambda: self.add_weather('weather2'))
 
-        self.weather1_del_button.clicked.connect(lambda: self.del_weather(1))
-        self.weather2_del_button.clicked.connect(lambda: self.del_weather(2))
+        self.weather1_del_button.clicked.connect(lambda: self.del_weather('weather1'))
+        self.weather2_del_button.clicked.connect(lambda: self.del_weather('weather2'))
 
     def add_weather(self, which):
-        if which == 1:
-            weather = self.weather1.text()
-            if weather:
-                self.weather1_list.addItem(weather)
-        else:
-            weather = self.weather2.text()
-            if weather:
-                self.weather2_list.addItem(weather)
+        weather = getattr(self, which).text()
+        if weather:
+            getattr(self, which+'_list').addItem(weather)
 
     def del_weather(self, which):
-        if which == 1:
-            item = self.weather1_list.takeItem(self.weather1_list.currentRow())
-        else:
-            item = self.weather2_list.takeItem(self.weather2_list.currentRow())
+        weather_list = getattr(self, which+'_list')
+        item = weather_list.takeItem(weather_list.currentRow())
         item = None
 
 
