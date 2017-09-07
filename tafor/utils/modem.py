75605fe74cd4bcb72362ba9023ce9d88c01dfbaa
piratecb <piratecb@gmail.com>
2017-09-07 19:27:44 +0800

    增加串口发送数据

---
+++ tafor/utils/modem.py
@@ -0,0 +1,14 @@
+from serial import Serial
+from serial import SerialException, SerialTimeoutException
+
+
+def serial_send(message, port):
+    with Serial(port, timeout=1) as ser:
+        message = bytes(message, 'utf-8')
+        count = ser.write(message)
+        return count == len(message)
+
+
+if __name__ == '__main__':
+    s = serial_send('TAF ZJHK 201004Z 201221 11004MPS 9999 SCT020 TX28/12Z TN26/21Z=', 'COM4')
+    print(s)
