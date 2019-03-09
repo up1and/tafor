6a4bd80647bf7ce770cd603af16c88420db94cad
up1and <piratecb@gmail.com>
2019-03-09 18:44:24 +0800

    新增远程报文接口转换报文为国际标准格式

--- scripts/tafor.py
+++ scripts/tafor.py
@@ -14,6 +14,102 @@ root = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__, static_url_path='/static')
 
 
+def parse_day_hour(utc, day, hour):
+    day = int(day)
+    hour = int(hour)
+    if hour == 24:
+        time = datetime.datetime(utc.year, utc.month, day) + datetime.timedelta(days=1)
+    else:
+        time = datetime.datetime(utc.year, utc.month, day, hour)
+    return time
+
+def generate_day_hour(time, end=False):
+    if time.hour == 0 and end:
+        time -= datetime.timedelta(hours=1)
+        text = '{}24'.format(str(time.day).zfill(2))
+    else:
+        text = '{}{}'.format(str(time.day).zfill(2), str(time.hour).zfill(2))
+
+    return text
+
+def parse_period(day, period):
+    utc = datetime.datetime.utcnow()
+    start = parse_day_hour(utc, day, period[:2])
+    end = parse_day_hour(utc, day, period[2:])
+    if end <= start:
+        end += datetime.timedelta(days=1)
+
+    return start, end
+
+def parse_intl_period(period):
+    start, end = period.split('/')
+    utc = datetime.datetime.utcnow()
+    start = parse_day_hour(utc, start[:2], start[2:])
+    end = parse_day_hour(utc, end[:2], end[2:])
+    return start, end
+
+def parse_basetime(basetime, hour):
+    if hour == '24':
+        time = datetime.datetime(basetime.year, basetime.month, basetime.day) + datetime.timedelta(days=1)
+    else:
+        time = datetime.datetime(basetime.year, basetime.month, basetime.day, int(hour))
+
+    if time < basetime:
+        time += datetime.timedelta(days=1)
+
+    return time
+
+def parse_interval(basetime, interval):
+    start = parse_basetime(basetime, interval[:2])
+    end = parse_basetime(basetime, interval[2:])
+    if end <= start:
+        end += datetime.timedelta(days=1)
+
+    return start, end
+
+def conversion(message):
+    message = message.replace('=', '')
+    items = message.split()
+    period_pattern = re.compile(r'(0[1-9]|[12][0-9]|3[0-1])(0009|0312|0615|0918|1221|1524|1803|2106|0024|0606|1212|1818)')
+    interval_pattern = re.compile(r'\b([01][0-9]|2[0-3])([01][0-9]|2[0-4])\b')
+    temp_pattern = re.compile(r'T(?:X|N)M?\d{2}/(\d{2})Z')
+
+    for i, item in enumerate(items):
+        m = period_pattern.match(item)
+        if m:
+            day, period = m.groups()
+            start, end = parse_period(day, period)
+            basetime = start
+            items[i] = '{}/{}'.format(generate_day_hour(start), generate_day_hour(end, end=True))
+
+        m = temp_pattern.match(item)
+        if m:
+            hour = m.group(1)
+            time = parse_basetime(basetime, hour)
+            day_hour = generate_day_hour(time)
+
+            if '{}0009'.format(day) in message:
+                day_hour = generate_day_hour(time)
+
+            if '{}1524'.format(day) in message:
+                day_hour = generate_day_hour(time, end=True)
+
+            if '{}0024'.format(day) in message:
+                if hour == '00':
+                    day_hour = generate_day_hour(time)
+                if hour == '24':
+                    day_hour = generate_day_hour(time, end=True)
+
+            items[i] = '{}{}Z'.format(item[:-3], day_hour)
+
+        m = interval_pattern.match(item)
+        if m:
+            interval = m.group()
+            start, end = parse_interval(basetime, interval)
+            items[i] = '{}/{}'.format(generate_day_hour(start), generate_day_hour(end, end=True))
+
+    return ' '.join(items) + '='
+
 def find_key(message):
     if message.startswith('METAR'):
         return 'SA'
@@ -22,16 +118,31 @@ def find_key(message):
         return 'SP'
 
     if message.startswith('TAF'):
-        pattern = re.compile(r'(0[1-9]|[12][0-9]|3[0-1])(0024|0606|1212|1818)')
-        intl_pattern = re.compile(r'(\d{4})/(\d{4})')
-        if pattern.search(message) or intl_pattern.search(message):
-            return 'FT'
-        else:
+        ft_pattern = re.compile(r'(0[1-9]|[12][0-9]|3[0-1])(0024|0606|1212|1818)')
+        fc_pattern = re.compile(r'(0[1-9]|[12][0-9]|3[0-1])(0009|0312|0615|0918|1221|1524|1803|2106)')
+        international_pattern = re.compile(r'(\d{4})/(\d{4})')
+
+        if fc_pattern.search(message):
             return 'FC'
 
-def marshal(messages):
+        if ft_pattern.search(message):
+            return 'FT'
+
+        m = international_pattern.search(message)
+        if m:
+            start, end = parse_intl_period(m.group())
+            if end - start < datetime.timedelta(hours=24):
+                return 'FC'
+            else:
+                return 'FT'
+
+def marshal(messages, international_mode=False):
     resp_dict = {}
     for message in messages:
+        pattern = re.compile(r'(0[1-9]|[12][0-9]|3[0-1])(0009|0312|0615|0918|1221|1524|1803|2106|0024|0606|1212|1818)')
+        if international_mode and pattern.search(message):
+            message = conversion(message)
+
         resp_dict[find_key(message)] = message
 
     return resp_dict
@@ -102,7 +213,7 @@ def remote_latest(airport):
         response = requests.post(url, params=post_data, timeout=30)
         messages = [msg['RPT'].strip().replace('\n', ' ') for msg in response.json()]
 
-        return jsonify(marshal(messages))
+        return jsonify(marshal(messages, international_mode=True))
 
     except Exception as e:
         app.logger.exception(e)
