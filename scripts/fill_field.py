55465cb0641d42fd63dfbbc9d8f0c40d9b480d0f
up1and <piratecb@gmail.com>
2022-06-27 11:05:40 +0800

    完善数据库迁移脚本

--- scripts/fill_field.py
+++ scripts/fill_field.py
@@ -3,6 +3,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from uuid import uuid4
+
 from tafor.models import db, Taf, Trend, Sigmet, Other
 
 
@@ -27,10 +29,20 @@ def addSource(model):
 
     db.commit()
 
+def addUuid(model):
+    items = db.query(model).filter(model.uuid == None).all()
+    for item in items:
+        item.uuid = str(uuid4())
+        
+        db.add(item)
+
+    db.commit()
+
 def main():
     for model in [Taf, Trend, Sigmet, Other]:
         addProtocol(model)
         addSource(model)
+        addUuid(model)
     
 
 if __name__ == '__main__':
